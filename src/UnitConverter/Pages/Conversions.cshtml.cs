3ef37829fdb0fa0bbd7cb411279c43d24f90f03e
JaceNWolfe <awolfe14@wvup.edu>
2026-09-09 15:39:03 -0400

    Started Adding route-based UnitOf conversions and error handling

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -1,26 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using UnitOf;
 namespace UnitConverter.Pages;
 
 public class ConversionsModel : PageModel
 {
+    [BindProperty(SupportsGet = true)]
     public string Input { get; set; } = string.Empty;
 
     public string Output { get; set; } = string.Empty;
 
+    [BindProperty(SupportsGet = true)]
+    public string ConversionType { get; set; }  = "MilesToKilometers";
+
+
     public void OnGet()
     {
-        Input = "3.1415";
+        ViewData["ConversionType"] = ConversionType;
+        ViewData["Title"] = "Conversions - " + ConversionType;
 
-        ViewData["ConversionType"] = "Miles to Kilometers";
-        ViewData["Title"] = "Conversions";
+        double value = Convert.ToDouble(Input);
 
-        double miles = Convert.ToDouble(Input);
+        // switch expression on conversionType
 
-        double kilometers = new UnitOf.Length()
-            .FromMiles(miles)
-            .ToKilometers();
-
-        Output = kilometers.ToString();
+        switch (ConversionType.ToLower())
+        {
+            case "milestokilometers":
+                Output = new UnitOf.Length().FromMiles(value).ToKilometers().ToString();
+                    break;
+            case "kilometerstomiles":
+                Output = new UnitOf.Length().FromMiles(value).ToKilometers().ToString();
+                    break;
+            case "fahrenheittocelsius":
+                Output = new UnitOf.Temperature().FromFahrenheit(value).ToCelsius().ToString();
+                    break;
+            case "celsiustofahrenheit":
+                Output = new UnitOf.Temperature().FromCelsius(value).ToFahrenheit().ToString();
+                    break;
+            case "poundstokilograms":
+                Output = new UnitOf.Mass().FromPounds(value).ToKilograms().ToString();
+                    break;
+            case "kilogramstopounds":
+                Output = new UnitOf.Mass().FromKilograms(value).ToPounds().ToString();
+                    break;
+            default:
+                ViewData["ErrorMessage"] = "Unknown conversion type. Please check spelling";
+                break;
+        }
     }
 }
